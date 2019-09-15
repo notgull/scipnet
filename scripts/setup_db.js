dc952579abae50a27b6d6c91b6ada72cdc17bdf7
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-15 19:09:15 -0400

    Use interp for setup_db.js

--- scripts/setup_db.js
+++ scripts/setup_db.js
@@ -21,16 +21,18 @@
  */
 
 // attempt to load configuration
-var config;
+let config;
 try {
   config = require("config.json");
 } catch(_e) {
   config = require("../config.json");
 }
 
-var username = config.postgres_username;
-var password = config.postgres_password;
-var database = config.postgres_database;
+const username = config.postgres_username;
+const password = config.postgres_password;
+const database = config.postgres_database;
 
-console.log("CREATE USER " + username + " PASSWORD '" + password + "';");
-console.log("CREATE DATABASE " + database + " OWNER = " + username + ";");
+console.log(`
+  CREATE USER ${username} PASSWORD '${password}';
+  CREATE DATABASE ${database} OWNER = ${username};
+`);
