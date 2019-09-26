340bff64f3f4f327f2c261fc960763c02c0ed212
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-26 01:11:20 -0400

    Rename to files.data.diff

--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,7 @@ function check_dir(dirname: string) {
 
 check_dir(config.get('files.data.content'));
 check_dir(config.get('files.data.metadata'));
-check_dir(config.get('scp_diff_location'));
+check_dir(config.get('files.data.diff'));
 check_dir(config.get('scp_files_location'));
 
 // load up the SQL before we start up
