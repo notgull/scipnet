881551a8b6d34c010b20d90cd87f6e04bb98bd4f
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-25 22:24:28 -0400

    Replace individual error constants with enum.

---
+++ server/errors.ts
@@ -0,0 +1,28 @@
+/*
+ * errors.ts
+ *
+ * scipnet - SCP Hosting Platform
+ * Copyright (C) 2019 not_a_seagull, Ammon Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+export enum ErrorCode {
+  INTERNAL_ERROR = 3,
+  USER_NOT_FOUND = 5,
+  PASSWORD_INCORRECT = 7,
+  SESSION_MISMATCH = 11,
+  SESSION_EXPIRY = 13,
+  EMAIL_NOT_FOUND = 17,
+}
