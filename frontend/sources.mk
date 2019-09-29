0265e884186b05fce20b741eaa9886037edd943c
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-28 23:08:04 -0400

    Add sourcing to Makefiles.

--- frontend/sources.mk
+++ frontend/sources.mk
@@ -1,5 +1,5 @@
 #
-# Makefile
+# sources.mk
 #
 # scipnet - SCP Hosting Platform
 # Copyright (C) 2019 not_a_seagull, Ammon Smith
@@ -17,34 +17,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-include frontend/sources.mk
-include backend/sources.mk
-
-ARTIFACTS := \
-	ftml-json/target/release/ftml-json \
-	frontend/dist/404.js \
-	backend/dist/index.js
-
-.PHONY: default prepare clean clean-all
-
-default: $(ARTIFACTS)
-
-prepare:
-	make -C frontend prepare
-	make -C backend prepare
-
-ftml-json/target/release/ftml-json: ftml-json/Cargo.toml ftml-json/src/*
-	cd ftml-json && cargo build --release
-
-frontend/dist/404.js: $(FRONTEND_SOURCES)
-	make -C frontend
-
-backend/dist/index.js: $(BACKEND_SOURCES)
-	make -C backend
-
-clean:
-	make -C frontend clean
-	make -C backend clean
-
-clean-all: clean
-	cd ftml-json && cargo clean
+FRONTEND_SOURCES := \
+	frontend/Makefile \
+	frontend/sources.mk \
+	frontend/gulpfile.js \
+	frontend/package.json \
+	frontend/tsconfig.json \
+	frontend/*.js \
+	frontend/*.ts
