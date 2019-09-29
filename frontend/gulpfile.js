89e77f27ce2e03f26e35e326c3026aa165e18181
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-28 21:18:46 -0400

    Update header information.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,8 +1,8 @@
 /*
  * gulpfile.js
  *
- * scipnet - SCP Hosting Platform
- * Copyright (C) 2019 not_a_seagull
+ * scipnet - Multi-tenant writing wiki software
+ * Copyright (C) 2019 not_a_seagull, Ammon Smith
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
