9c34b60460ab3a512ddd54176145ec1f2dc593dc
Ammon Smith <ammon.i.smith@gmail.com>
2019-10-10 21:00:47 -0400

    Setup helper sql functions.

--- backend/src/sql.ts
+++ backend/src/sql.ts
@@ -18,23 +18,75 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// exposes basic SQL functionality
-import { Pool } from 'pg';
-import * as path from 'path';
-
 import { config } from 'app/config';
+import { Nullable } from 'app/utils';
 
-const pool = new Pool({
-  user: config.get('database.username'),
-  host: config.get('database.host'),
-  database: config.get('database.name'),
-  password: config.get('database.password'),
-}); // TODO: set up port?
+import { Pool } from 'pg';
 
-export function query(sql: string, args: any, callback: (err: any, res: any) => void): void {
-  pool.query(sql, args, callback);
+import * as knex from 'knex';
+
+export type SqlType =
+  | string
+  | number
+  | Date
+  | object
+  ;
+
+let pool: Nullable<Pool> = null;
+
+export class DatabaseError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'DatabaseError';
+  }
 }
 
-export async function queryPromise(sql: string, args: any): Promise<any> {
-  return pool.query(sql, args);
+function createPool() {
+  const host = config.get('database.host');
+  const user = config.get('database.username');
+  const password = config.get('database.password');
+  const database = config.get('database.name');
+  const timeout = config.get('database.timeout');
+  const maxThreads = config.get('database.max_threads');
+
+  const url = `postgresql://${user}:${password}@${host}/${database}`;
+
+  return new Pool({
+    user,
+    host,
+    database,
+    password,
+    max: maxThreads,
+    connectionTimeoutMillis: timeout,
+    idleTimeoutMillis: timeout,
+  });
 }
+
+export async function rawQuery(sql: string, params: SqlType[]): Promise<any> {
+  if (pool == null) {
+    pool = createPool();
+  }
+
+  return pool.query(sql, params);
+}
+
+export async function findOne<T>(sql: string, params: SqlType[]): Promise<Nullable<Partial<T>>> {
+  const result = await rawQuery(sql, params);
+
+  switch (result.rowCount) {
+    case 0:
+      return null;
+    case 1:
+      return result.rows[0];
+    default:
+      throw new DatabaseError('Primary key query returned multiple results');
+  }
+}
+
+export async function findMany<T>(sql: string, params: SqlType[]): Promise<Array<Partial<T>>> {
+  const result = await rawQuery(sql, params);
+  return result.rows;
+}
+
+// deprecate
+export const queryPromise = rawQuery;
