b0d5a9a207aac1cd54b17fb842654959e8d67c46
Ammon Smith <ammon.i.smith@gmail.com>
2019-10-11 21:06:07 -0400

    Change digest to VARCHAR.

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -23,19 +23,6 @@ import { findOne, DatabaseError } from 'app/sql';
 import { PasswordModel } from 'app/sql/models';
 import { timeout, Nullable } from 'app/utils'
 
-function getDigest(digestNo: number): string {
-  // Not using string formatting so we can accomodate non-SHA digests.
-  // Also no SHA1 since it's insecure.
-  switch (digestNo) {
-    case 224: return 'sha224';
-    case 256: return 'sha256';
-    case 384: return 'sha384';
-    case 512: return 'sha512';
-    default:
-      throw new DatabaseError(`Passed digest number is not known: ${digestNo}`);
-  }
-}
-
 // Represents a password as stored in the database, with utility methods.
 // The actual plaintext is never stored anywhere.
 export class Password {
@@ -67,7 +54,7 @@ export class Password {
       model.salt,
       model.iterations,
       model.key_size,
-      getDigest(model.digest),
+      model.digest,
     );
   }
 
