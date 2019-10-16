3b3c7fbda3eab1dfa066eb878f3cde9e4bff901a
not_a_seagull <notaseagull048@gmail.com>
2019-10-16 13:40:55 -0700

    Set up JSONRPC server, now need to setup handlers

---
+++ backend/src/server/README.txt
@@ -0,0 +1,26 @@
+=== ScipnetJsonApp SERVER API ===
+
+scipnet is a JSON-RPC-2.0 server that will response to the following commands:
+
+* /favicon.ico - returns the favicon, standard stuff for a web server
+* /sys/login, /sys/register - returns either the login or register pages, in html
+* /sys/process-login - takes the information in the Body section of the request in order to log the user in
+* /sys/process-register - takes the information in the Body section of the request to create a new user object
+* /sys/pagereq - pagereq, params should be in the Body section
+* /sys/fonts/* - loads a font
+* /sys/images/* - loads an image
+* / - loads the main page
+* /{slug} - loads a page by its slug
+
+Params should contain the following:
+
+* ip - IP Address of the client
+* body - variables from the Body section, in the form of a dict
+* params - URL params
+* cookies - cookies
+
+Return value will be:
+
+* data - JSON object containing data from the procedure
+* send - either null or a string containing HTML data to be displayed in the browser
+* redirect - either null or a url to redirect to
--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -22,6 +22,7 @@
 import * as fs from "fs";
 import * as http from "http";
 import * as https from "https";
+import * as jaysom from "jayson/promise";
 import * as path from "path";
 
 import { promisify } from "util";
@@ -34,53 +35,35 @@ import { Usertable } from "app/services/user/usertable";
 // types for the required functions
 export type ScipnetStringMap = { [key: string]: string };
 export type ScipnetMap = { [key: string]: any };
-export interface ScipnetRequest {
+export interface ScipnetInformation {
   body?: ScipnetMap, // POST request body
   cookies?: ScipnetStringMap, // cookies
-//  query?: ScipnetStringMap, // GET request URL params, should not be used
   params?: ScipnetMap, // /url/params/like/this
 
   method: string,
   ip: string
 };
-export interface ScipnetResponse {
-  currentHeader?: ScipnetStringMap,
+export interface ScipnetOutput = {
+  _redirect: Nullable<string>,
+  _send: Nullable<string | Buffer>,
 
-  cookie(name: string, value: string, age: number): void,
-  redirect(url: string): void,
-  send(data: string | Buffer, code?: number): void,
-  type(mimeType: string): void
+  redirect: (url: string),
+  send: (data: string | Buffer),
 };
 
-export type SyncScipnetHandle = (req: ScipnetRequest, res: ScipnetResponse, ut?: Usertable) => void;
-export type AsyncScipnetHandle = (req: ScipnetRequest, res: ScipnetResponse, ut?: Usertable) => Promise<void>;
+export type SyncScipnetHandle = (inf: ScipnetInformation, out: ScipnetOutput, ut?: Usertable) => any;
+export type AsyncScipnetHandle = (inf: ScipnetInformation, out: ScipnetOutput, ut?: Usertable) => Promise<any>;
 export type ScipnetHandle = SyncScipnetHandle | AsyncScipnetHandle;
 
 export type ScipnetFunctionMap = { [key: string]: ScipnetHandle };
 
 // some basic handles
-const faviconHandle: ScipnetHandle = async function(req: ScipnetRequest, res: ScipnetResponse) {
-  let faviconSource = await readFilePromise(config.get("files.images.favicon"));
-  res.send(faviconSource);
+const faviconHandle: ScipnetHandle = async function(inf: ScipnetInformation, out: ScipnetOutput): Promise<string> {
+  const faviconSource = await readFilePromise(config.get("files.images.favicon"));
+  return faviconSource;
 };
 
-// helper function for parsing cookies
-function parseCookies(req: http.ClientRequest): ScipnetStringMap {
-  let list: ScipnetStringMap = {};
-  let rc = req.getHeader("cookie");
-  if (!(rc instanceof String)) { throw new Error("Need to handle cookie edge cases"); }
-
-  if (rc) {
-    rc.split(';').forEach(function(cookie: string) {
-      const parts = cookie.split('=');
-      list[parts.shift().trim()] = decodeURI(parts.join('='));
-    });
-  }
-
-  return list;
-}
-
-export class ScipnetHttpsApp { 
+export class ScipnetJsonApp { 
   basicPageHandle: Nullable<ScipnetHandle>;
   faviconHandle: ScipnetHandle;
  
@@ -118,162 +101,81 @@ export class ScipnetHttpsApp {
     this.usertable = new Usertable();
   }
 
-  // call a scipnet handle
-  async executeHandle(handle: ScipnetHandle, req: ScipnetRequest, res: ScipnetResponse): Promise<void> { 
-    await handle(req, res, this.usertable); 
-  }
+  // wrapper for scipnet handles
+  wrapHandle(handle: ScipnetHandle): (args: any) => Promise<any> { 
+    const scopedThis = this;
 
-  // make it callable
-  getServer(): (req: any, res: any) => Promise<void> {
-    const scoped_this = this;
-
-    return async function(req: any, res: any): Promise<void> {
-      // get the url
-      let accessedUrl = req.url;
-      if (accessedUrl[0] === "/") {
-        accessedUrl = accessedUrl.substr(1);
-      }
-
-      let urlParts = accessedUrl.split("/");
-      const accessedPage = urlParts[0];
-
-      // determine if we are accessing a /sys/ page
-      urlParts.splice(0, 1);
-      let sysUrl = "";
-      if (accessedPage === "sys") {
-        sysUrl = urlParts[0];
-        urlParts.splice(0, 1);
-      }
-
-      // define request and response objects
-      let sReq: ScipnetRequest = { 
-        method: req.method,
-        ip: req.getHeader("x-forwarded-for") || req.connection.remoteAddress
-      };  
-      let sRes: ScipnetResponse = {
-        currentHeader: {
-          "Content-Type": "text/html"
+    return async function(args: any): Promise<any> {
+      // get function information
+      const inf: ScipnetInformation = {
+        body: params.body,
+        params: params.params,
+        cookies: params.cookies,
+        ip: params.ip
+      };
+      let output: ScipnetOutput = {
+        _send: null,
+        _redirect: null,
+  
+        redirect: function(url: string) {
+          output._redirect = url;
         },
-
-        cookie: function(cookieName: string, cookieValue: string, age: number) {
-          // TODO: set cookie
-        },
-         
-        redirect: function(url: string)  {
-          sRes.currentHeader["Location"] = url;
-          res.writeHead(302, sRes.currentHeader);
-          res.end();
-        },
-
-        send: function(data: Buffer | string, code: number = 200) {
+        send: function(data: string | Buffer) {
           if (data instanceof Buffer) {
-            data = data.toString();
+            output._send = data.toString();
+          } else {
+            output._send = data;
           }
-
-          sRes.currentHeader["Content-Length"] = `${Buffer.byteLength(data)}`;
-          res.writeHead(code, sRes.currentHeader);
-          res.end(data);
-        },
-
-        type: function(mimeType: string) {
-          sRes.currentHeader["Content-Type"] = mimeType;
         }
       };
 
-      // add cookies
-      sReq.cookies = parseCookies(req);    
-
-      // add params from url
-      let params: ScipnetMap = {};
-      let param: any;
-      let unmodifiedParam: any;
-      if (params.length > 1) {
-        for (let i = 0; i < params.length; i += 2) { // iterate two at a time
-          // determine value of param
-          param = urlParts[i + 1];
-          if (!param) { break; } // TODO: handle this edge case better
-          unmodifiedParam = param;      
-
-          // TODO: I feel like there's a more simple way of doing this
-          param = parseInt(param);
-          if (param === NaN) {
-            if (param === "true") {
-              param = true;
-            } else if (param === "false") {
-              param = false;
-            } else {
-              param = unmodifiedParam;
-            }
-          }
-
-          params[urlParts[i]] = param;
-        }
-      }
-      sReq.params = params;
-
-      // may God forgive me for what I'm about to do
-      const that: ScipnetHttpsApp = scoped_this;
-
-      // at this point, we should be able to access the request's body
-      let body = "";
-
-      req.on("data", (chunk: Buffer) => { body += chunk.toString(); });
-      req.on("end", async () => {
-        // body is likely to be encoded via url encoding; however, there is a chance for JSON encoding
-        try {
-          sReq.body = querystring.parse(body);
-        } catch(e) {
-          sReq.body = JSON.parse(body);
-        }
-
-        // now that all of the parameters are loaded we can see which handle we need to execute
-        if (accessedPage === "sys") {
-          if (sReq.method === "GET") {
-            switch (sysUrl) {
-              case "bundle.js":
-                await that.executeHandle(that.bundleHandle, sReq, sRes);
-                return;
-              case "images":
-                await that.executeHandle(that.imageHandles[urlParts[0]], sReq, sRes);
-                return;
-              case "fonts":
-                await that.executeHandle(that.fontHandles[urlParts[0]], sReq, sRes);
-                return;
-              case "login":
-                await that.executeHandle(that.loginHandle, sReq, sRes);
-                return;
-              case "register":
-                await that.executeHandle(that.registerHandle, sReq, sRes);
-                return;
-              default:
-                sRes.redirect("/");
-                return;
-            }
-          } else if (sReq.method === "POST") {
-            switch (sysUrl) {
-              case "pagereq":
-                await that.executeHandle(that.pagereqHandle, sReq, sRes);
-                return;
-              case "process-login":
-                await that.executeHandle(that.processLoginHandle, sReq, sRes);
-                return;
-              case "process-register":
-                await that.executeHandle(that.processRegisterHandle, sReq, sRes);
-                return;
-              default:
-                sRes.send("Unable to find desired POST target", 404);
-                return;
-            } 
-          } else {
-            sRes.send(`Method ${sReq.method} is not supported`, 404);
-          }
-        } else if (accessedPage === "") {
-          await that.executeHandle(that.mainHandle, sReq, sRes);
-        } else {
-          sReq.body.pageid = accessedPage;
-          await that.executeHandle(that.pageHandle, sReq, sRes);
-        }
-      });
+      const data = await handle(inf, output, scopedThis.usertable);
+      return {
+        data: data,
+        send: output._send,
+        redirect: output._redirect
+      };
     };
+  }
+
+  // run jayson server
+  runServer() {
+    // create parameters for the server
+    let rpcFunctions: ScipnetFunctionMap = {
+      "/favicon.ico": this.wrapHandle(this.faviconHandle),
+      "/": this.wrapHandle(this.mainHandle),
+      "/sys/login": this.wrapHandle(this.loginHandle),
+      "/sys/register": this.wrapHandle(this.registerHandle),
+      "/sys/process-login": this.wrapHandle(this.processLoginHandle),
+      "/sys/process-register": this.wrapHandle(this.processRegisterHandle),
+      "/sys/pagereq": this.wrapHandle(this.pagereqHandle)
+    };
+    for (const font of this.fontHandles) {
+      rpcFunctions[`/sys/fonts/${font}`] = this.wrapHandle(this.fontHandles[font]);
+    }
+    for (const image of this.imageHandles) {
+      rpcFunctions[`/sys/images/${image}`] = this.wrapHandle(this.imageHandles[image]);
+    }
+
+    const wrappedPageHandle = this.wrapHandle(this.
+
+    // create the server itself
+    const server = jayson.server(rpcFunctions, {
+      router: function(method: string, params: any): jayson.Method {
+        // do by-name routing first
+        if (typeof(this._methods[method]) === "function") return this._methods[method];
+        
+        if (method[0] !== "/") { 
+          method = `/${method}`;
+        }
+
+        // look for page
+        const methodParts = method.split("/");
+        params.pageid = methodParts[methodParts.length - 1];
+        return wrappedPageHandle;
+      }
+    }); 
+
+    server.http().listen(config.get("services.scipnet.port"));
   }
 }
